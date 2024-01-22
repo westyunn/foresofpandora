7c4384597173bbd949187f1b6a4b0a0aaa8fabd6
김성재 <sngjae972@gmail.com>
2024-01-22 17:19:02 +0900

    backend init

---
+++ backend/src/main/java/com/ssafy/forest/domain/entity/Member.java
@@ -0,0 +1,25 @@
+package com.ssafy.forest.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Integer id;
+
+}
