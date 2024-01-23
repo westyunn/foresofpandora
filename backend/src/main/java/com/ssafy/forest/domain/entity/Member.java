4619140ce1c5fd0fab9f4586a2fc57827a15f339
김성재 <sngjae972@gmail.com>
2024-01-23 16:46:34 +0900

    refactor: Member 엔티티 컬럼 추가

--- backend/src/main/java/com/ssafy/forest/domain/entity/Member.java
+++ backend/src/main/java/com/ssafy/forest/domain/entity/Member.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,5 +22,8 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "member_id")
     private Long id;
+
+    private String email;
+    private LocalDateTime deletedAt;
 
 }
