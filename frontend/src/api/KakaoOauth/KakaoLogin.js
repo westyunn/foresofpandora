92138fd9ca7b1e84353837504122f6d459e1c3ae
이서윤 <s2yunn@gmail.com>
2024-01-24 16:55:31 +0900

    feat: assets 아이콘 추가, 소셜 로그인 수정

--- frontend/src/api/KakaoOauth/KakaoLogin.js
+++ frontend/src/api/KakaoOauth/KakaoLogin.js
@@ -2,35 +2,38 @@ import axios from "axios";
 import { useEffect } from 'react';
 import { Link, useNavigate } from "react-router-dom";
 
-const KakaoLogin = (prop) => {
-    const navigate = useNavigate();
+const Login = () => {
+const navigate = useNavigate();
+    const code = new URL(document.location.toString()).searchParams.get("code");
    useEffect(() => {
     const login = async() => {
+        // 백으로 인가코드 넘겨주기
         await axios({
             method: 'GET',
-            url: `api/kakao/login?code=${prop.cname}`,
+            url: `/api/auth/kakao/login?code=${code}`,
             headers: {
                 "Content-Type": "application/json;charset=utf-8", //json형태로 데이터를 보내겠다는뜻 
             }
         }).then((res) => {
             console.log(res);
             // localStorage.setItem("email", res.data.account.kakaoEmail);
-            navigate("kakaologin");
+            navigate("/");
         }).catch((err)=>{
             console.log(err);
         })
     };
     login();
-   }, [prop.cname, navigate ])
+   }, [code, navigate ])
 
     // const response = await axios.get()
     return (
         <>
-        <button href={prop.url}>카카오 로그인</button>
-        <Link to="/login"></Link>
+        <div>
+            개발중...
+        </div>
         </>
     )
 
 }
 
-export default KakaoLogin;
+export default Login;
