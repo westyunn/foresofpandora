3541e9796d38e10125f1fcb091426c8306b05bff
안성재 <sammy0329@naver.com>
2024-01-26 17:37:06 +0900

    feat:ArticleCommentRepository 작성

--- backend/src/main/java/com/ssafy/forest/repository/ArticleRepository.java
+++ backend/src/main/java/com/ssafy/forest/repository/ArticleRepository.java
@@ -1,6 +1,9 @@
 package com.ssafy.forest.repository;
 
 import com.ssafy.forest.domain.entity.Article;
+import com.ssafy.forest.domain.entity.ArticleComment;
+import com.ssafy.forest.domain.entity.Member;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
