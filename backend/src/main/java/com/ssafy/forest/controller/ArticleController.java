9a80f83fda1bbc2770936102c5b09c209b280f5e
윤정인 <liae100@naver.com>
2024-01-29 09:27:41 +0900

    refactor : ArticleController 메소드 수정
    - 메소드 이름 crud 형식으로 변경
    - 게시글 등록 수정

--- backend/src/main/java/com/ssafy/forest/controller/ArticleController.java
+++ backend/src/main/java/com/ssafy/forest/controller/ArticleController.java
@@ -7,7 +7,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,41 +25,41 @@ public class ArticleController {
 
     //게시글 등록
     @PostMapping
-    public ResponseEntity<Void> registArticle(@RequestBody ArticleReqDto request) {
+    public ResponseEntity<Void> create(@RequestBody ArticleReqDto request) {
         System.out.println("시작");
-        articleService.registArticle(1L, request);
+        articleService.create(1L, request);
         System.out.println("끝");
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     //게시글 목록 조회
     @GetMapping
-    public ResponseEntity<List<ArticleResDto>> getArticleList() {
-        List<ArticleResDto> list = articleService.getArticleList();
+    public ResponseEntity<List<ArticleResDto>> readList() {
+        List<ArticleResDto> list = articleService.readList();
         return ResponseEntity.ok(list);
     }
 
     //게시글 단건 조회
     @GetMapping("/{articleId}")
-    public ResponseEntity<ArticleResDto> getArticle(@PathVariable Long articleId) {
+    public ResponseEntity<ArticleResDto> read(@PathVariable Long articleId) {
         System.out.println("시작");
-        ArticleResDto response = articleService.getArticle(articleId);
+        ArticleResDto response = articleService.read(articleId);
         System.out.println("종료");
         return ResponseEntity.ok(response);
     }
 
     //게시글 수정
     @PutMapping("/{articleId}")
-    public ResponseEntity<Void> updateArticle(@PathVariable Long articleId,
+    public ResponseEntity<Void> update(@PathVariable Long articleId,
         @RequestBody ArticleReqDto request) {
-        articleService.updateArticle(articleId, request);
+        articleService.update(articleId, request);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
     //게시글 삭제
     @DeleteMapping("/{articleId}")
-    public ResponseEntity<Void> deleteArticle(@PathVariable Long articleId) {
-        articleService.deleteArticle(articleId);
+    public ResponseEntity<Void> delete(@PathVariable Long articleId) {
+        articleService.delete(articleId);
         return ResponseEntity.status(HttpStatus.OK).build();
     }
 
