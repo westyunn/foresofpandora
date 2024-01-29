7ca70c1b2fa3d68f75c6355ebb9c0290a421dfef
김성재 <sngjae972@gmail.com>
2024-01-29 13:44:16 +0900

    refactor: 토큰으로부터 Member를 조회하는 예제 코드 추가

--- backend/src/main/java/com/ssafy/forest/controller/AuthController.java
+++ backend/src/main/java/com/ssafy/forest/controller/AuthController.java
@@ -1,33 +1,49 @@
 package com.ssafy.forest.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ssafy.forest.domain.dto.response.MemberResDto;
 import com.ssafy.forest.domain.dto.response.ResponseDto;
 import com.ssafy.forest.service.KakaoOauthService;
+import com.ssafy.forest.service.TestService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 //@RestController
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
 
     private final KakaoOauthService kakaoOauthService;
+    private final TestService testService;
 
     @GetMapping("/index")
     public String index() {
         return "loginForm";
     }
 
-    @GetMapping("/auth/kakao")
+    @GetMapping("/success")
+    public String success() {
+        return "loginSuccess";
+    }
+
+    @GetMapping("/kakao")
     @ResponseBody
-    public ResponseDto<?> kakaoLogin(@RequestParam("code") String code,
+    public ResponseDto<MemberResDto> kakaoLogin(@RequestParam("code") String code,
         HttpServletRequest request, HttpServletResponse response) throws JsonProcessingException {
         return ResponseDto.success(kakaoOauthService.kakaoLogin(code, request, response));
+    }
+
+    @GetMapping("/test")
+    @ResponseBody
+    public ResponseDto<MemberResDto> getMemberFromAccessToken(HttpServletRequest request) {
+        return ResponseDto.success(testService.getMemberFromAccessToken(request));
     }
 
 }
