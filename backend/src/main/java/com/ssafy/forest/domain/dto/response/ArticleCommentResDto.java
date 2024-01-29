0463c6f2ea5283d3e8362b5b7db8d221a01609f6
안성재 <sammy0329@naver.com>
2024-01-29 22:20:30 +0900

    feat:ArticleComment Response DTO 개발

---
+++ backend/src/main/java/com/ssafy/forest/domain/dto/response/ArticleCommentResDto.java
@@ -0,0 +1,34 @@
+package com.ssafy.forest.domain.dto.response;
+
+import com.ssafy.forest.domain.entity.ArticleComment;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class ArticleCommentResDto {
+
+    private Long commentId;
+    private Long articleId;
+    private Long memberId;
+    private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
+
+    public static ArticleCommentResDto from(ArticleComment articleComment) {
+        return new ArticleCommentResDto(
+            articleComment.getId(),
+            articleComment.getArticle().getId(),
+            articleComment.getMember().getId(),
+            articleComment.getContent(),
+            articleComment.getCreatedAt(),
+            articleComment.getModifiedAt()
+
+        );
+    }
+
+}
