6704719b74c239e46dc7402868b69f4edc973971
윤정인 <liae100@naver.com>
2024-01-30 12:27:01 +0900

    feat : findByMemberId 기능 추가
    - findByMemberId 기능 추가

--- backend/src/main/java/com/ssafy/forest/repository/ArticleRepository.java
+++ backend/src/main/java/com/ssafy/forest/repository/ArticleRepository.java
@@ -1,6 +1,7 @@
 package com.ssafy.forest.repository;
 
 import com.ssafy.forest.domain.entity.Article;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,4 +10,6 @@ public interface ArticleRepository extends JpaRepository<Article, Long> {
     Optional<Article> findById(Long articleId);
 
     public void deleteById(Long articleId);
+
+    List<Article> findByMemberId(Long memberId);
 }
