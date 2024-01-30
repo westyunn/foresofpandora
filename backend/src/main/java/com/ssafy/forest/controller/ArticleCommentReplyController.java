f68280a8d90650e8cb801f805f0528dce2cad538
안성재 <sammy0329@naver.com>
2024-01-30 22:19:25 +0900

    feat: getCommentReplies 메서드 Page 작업을 위해 변경
    - 페이지 작업을 위해 List를 Page로 변경
    - @PageableDefault 어노테이션을 통해 한번의 요청에 createdAt 기준으로 내림차순으로 10건씩 제공

--- backend/src/main/java/com/ssafy/forest/controller/ArticleCommentReplyController.java
+++ backend/src/main/java/com/ssafy/forest/controller/ArticleCommentReplyController.java
@@ -5,8 +5,11 @@ import com.ssafy.forest.domain.dto.response.ArticleCommentReplyResDto;
 import com.ssafy.forest.domain.dto.response.ResponseDto;
 import com.ssafy.forest.service.ArticleCommentReplyService;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,20 +24,20 @@ public class ArticleCommentReplyController {
 
     private final ArticleCommentReplyService articleCommentReplyService;
 
-    // commentReply 작성
     @PostMapping("/{commentId}/replies")
     public ResponseDto<ArticleCommentReplyResDto> create(
         HttpServletRequest request,
-        @PathVariable Long commentId, @RequestBody ArticleCommentReplyReqDto articleCommentReplyReqDto) {
+        @PathVariable Long commentId,
+        @RequestBody ArticleCommentReplyReqDto articleCommentReplyReqDto) {
         return ResponseDto.success(
             articleCommentReplyService.create(request, commentId, articleCommentReplyReqDto));
     }
 
-    // commentReply 목록 조회
     @GetMapping("/{commentId}/replies")
-    public ResponseDto<List<ArticleCommentReplyResDto>> getAllCommentReplies(
-        HttpServletRequest request, @PathVariable Long commentId) {
-        return ResponseDto.success(articleCommentReplyService.getCommentRepliesByComment(commentId));
+    public ResponseDto<Page<ArticleCommentReplyResDto>> getCommentReplies(
+        @PathVariable Long commentId,
+        @PageableDefault(size = 10, sort = "createdAt", direction = Direction.DESC) Pageable pageable) {
+        return ResponseDto.success(articleCommentReplyService.getCommentRepliesByComment(pageable, commentId));
     }
 
 }
