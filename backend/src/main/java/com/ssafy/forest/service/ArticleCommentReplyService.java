0a53878722a680d72ab5f198c7aa861558af8983
안성재 <sammy0329@naver.com>
2024-01-30 22:17:32 +0900

    feat: getCommentRepliesByComment 메서드 Page 작업을 위해 변경
    - 페이지 작업을 위해 List를 Page로 변경
    - pageable 파라미터 추가

--- backend/src/main/java/com/ssafy/forest/service/ArticleCommentReplyService.java
+++ backend/src/main/java/com/ssafy/forest/service/ArticleCommentReplyService.java
@@ -2,13 +2,15 @@ package com.ssafy.forest.service;
 
 import com.ssafy.forest.domain.dto.request.ArticleCommentReplyReqDto;
 import com.ssafy.forest.domain.dto.response.ArticleCommentReplyResDto;
-import com.ssafy.forest.domain.dto.response.ArticleCommentResDto;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface ArticleCommentReplyService {
 
-    ArticleCommentReplyResDto create(HttpServletRequest request, Long commentId, ArticleCommentReplyReqDto articleCommentReplyReqDto);
-    List<ArticleCommentReplyResDto> getCommentRepliesByComment(Long commentId);
+    ArticleCommentReplyResDto create(HttpServletRequest request, Long commentId,
+        ArticleCommentReplyReqDto articleCommentReplyReqDto);
+
+    Page<ArticleCommentReplyResDto> getCommentRepliesByComment(Pageable pageable, Long commentId);
+
 }
--- backend/src/main/java/com/ssafy/forest/service/ArticleCommentReplyServiceImpl.java
+++ backend/src/main/java/com/ssafy/forest/service/ArticleCommentReplyServiceImpl.java
@@ -2,8 +2,6 @@ package com.ssafy.forest.service;
 
 import com.ssafy.forest.domain.dto.request.ArticleCommentReplyReqDto;
 import com.ssafy.forest.domain.dto.response.ArticleCommentReplyResDto;
-import com.ssafy.forest.domain.dto.response.ArticleCommentResDto;
-import com.ssafy.forest.domain.entity.Article;
 import com.ssafy.forest.domain.entity.ArticleComment;
 import com.ssafy.forest.domain.entity.ArticleCommentReply;
 import com.ssafy.forest.domain.entity.Member;
@@ -14,10 +12,10 @@ import com.ssafy.forest.repository.ArticleCommentRepository;
 import com.ssafy.forest.repository.MemberRepository;
 import com.ssafy.forest.security.TokenProvider;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -56,14 +54,14 @@ public class ArticleCommentReplyServiceImpl implements ArticleCommentReplyServic
 
     @Override
     @Transactional(readOnly = true)
-    public List<ArticleCommentReplyResDto> getCommentRepliesByComment(Long commentId) {
+    public Page<ArticleCommentReplyResDto> getCommentRepliesByComment(Pageable pageable,
+        Long commentId) {
         // CommentId를 통해 Comment 엔티티 찾기
         ArticleComment articleComment = articleCommentRepository.findById(commentId)
             .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_COMMENT));
 
-        return articleCommentReplyRepository.findAllByArticleComment(articleComment).stream()
-            .map(ArticleCommentReplyResDto::from)
-            .collect(Collectors.toList());
+        return articleCommentReplyRepository.findAllByArticleCommentOrderByCreatedAt(pageable,
+            articleComment).map(ArticleCommentReplyResDto::from);
     }
 
 }
