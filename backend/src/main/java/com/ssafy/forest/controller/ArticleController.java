386fb87ce8c15e070b94fc25eca39621ee203b6d
윤정인 <liae100@naver.com>
2024-01-30 17:53:55 +0900

    feat : 게시글 목록 조회 페이징 기능 추가
    - 게시글 목록 조회 페이징 기능 추가

--- backend/src/main/java/com/ssafy/forest/controller/ArticleController.java
+++ backend/src/main/java/com/ssafy/forest/controller/ArticleController.java
@@ -5,10 +5,10 @@ import com.ssafy.forest.domain.dto.response.ArticleResDto;
 import com.ssafy.forest.domain.dto.response.ResponseDto;
 import com.ssafy.forest.service.ArticleService;
 import jakarta.servlet.http.HttpServletRequest;
-import java.awt.print.Pageable;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,9 +36,10 @@ public class ArticleController {
 
     //게시글 목록 조회
     @GetMapping
-    public ResponseDto<List<ArticleResDto>> readList(@RequestParam(defaultValue = "0") int page,
-        @RequestParam(defaultValue = "15") int size) {
-        List<ArticleResDto> articleList = articleService.readList(page, size);
+    public ResponseDto<Page<ArticleResDto>> readList(
+        @PageableDefault(size = 15, sort = "id", direction = Direction.ASC) Pageable pageable
+    ) {
+        Page<ArticleResDto> articleList = articleService.readList(pageable);
         return ResponseDto.success(articleList);
     }
 
