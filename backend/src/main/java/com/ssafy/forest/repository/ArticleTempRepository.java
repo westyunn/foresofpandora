eedaee108aa1707dfbf2cd9fff79f5adbc6a7dfd
윤정인 <liae100@naver.com>
2024-01-30 12:25:36 +0900

    feat : findByMemberId 추가
    - findByMemberId 기능 추가

--- backend/src/main/java/com/ssafy/forest/repository/ArticleTempRepository.java
+++ backend/src/main/java/com/ssafy/forest/repository/ArticleTempRepository.java
@@ -2,9 +2,11 @@ package com.ssafy.forest.repository;
 
 import com.ssafy.forest.domain.entity.Article;
 import com.ssafy.forest.domain.entity.ArticleTemp;
+import java.awt.print.Pageable;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ArticleTempRepository extends JpaRepository<ArticleTemp, Long> {
-
+    List<ArticleTemp> findByMemberId(Long memberId);
 }
