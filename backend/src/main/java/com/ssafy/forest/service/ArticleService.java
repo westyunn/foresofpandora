7ff95fa0737de81c1b6cc26ffb333a55391d8f1d
윤정인 <liae100@naver.com>
2024-01-30 13:07:00 +0900

    refactor : 접근 제한자 public 삭제
    - ArticleService 메소드 public 삭제
    - MemberService 메소드 public 삭제

--- backend/src/main/java/com/ssafy/forest/service/ArticleService.java
+++ backend/src/main/java/com/ssafy/forest/service/ArticleService.java
@@ -9,35 +9,35 @@ import java.util.List;
 public interface ArticleService {
 
     //게시글 등록
-    public ArticleResDto create(ArticleReqDto articleReqDto, HttpServletRequest request);
+    ArticleResDto create(ArticleReqDto articleReqDto, HttpServletRequest request);
 
     //게시글 목록 조회
-    public List<ArticleResDto> readList(int page, int size);
+    List<ArticleResDto> readList(int page, int size);
 
     //게시글 단건 조회
-    public ArticleResDto read(Long articleId);
+    ArticleResDto read(Long articleId);
 
     //게시글 수정
-    public ArticleResDto update(Long articleId, ArticleReqDto articleReqDto,
+    ArticleResDto update(Long articleId, ArticleReqDto articleReqDto,
         HttpServletRequest request);
 
     //게시글 삭제
-    public void delete(Long articleId, HttpServletRequest request);
+    void delete(Long articleId, HttpServletRequest request);
 
     //게시글 임시저장
-    public ArticleResDto createTemp(ArticleReqDto articleReqDto, HttpServletRequest request);
+    ArticleResDto createTemp(ArticleReqDto articleReqDto, HttpServletRequest request);
 
     //임시저장 게시글 단건 조회
-    public ArticleResDto readTemp(Long articleId);
+    ArticleResDto readTemp(Long articleId);
 
     //임시저장 게시글 등록
-    public ArticleResDto createTempToNew(Long tempId, ArticleReqDto articleReqDto,
+    ArticleResDto createTempToNew(Long tempId, ArticleReqDto articleReqDto,
         HttpServletRequest request);
 
     //임시저장 게시글 수정
-    public ArticleResDto updateTemp(Long articleId, ArticleReqDto articleReqDto,
+    ArticleResDto updateTemp(Long articleId, ArticleReqDto articleReqDto,
         HttpServletRequest request);
 
     //임시저장 게시글 삭제
-    public void deleteTemp(Long articleId, HttpServletRequest request);
+    void deleteTemp(Long articleId, HttpServletRequest request);
 }
