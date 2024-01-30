87c31ef2ea67800d525375f9cb25dd02a1507b5b
안성재 <sammy0329@naver.com>
2024-01-30 17:20:32 +0900

    feat: ArticleCommentReplyRepository에 findAllByArticleComment 메서드 추가
    - findAllByArticleComment: 해당 댓글에 대한 모든 대댓글을 들고오는 메서드

--- backend/src/main/java/com/ssafy/forest/repository/ArticleCommentReplyRepository.java
+++ backend/src/main/java/com/ssafy/forest/repository/ArticleCommentReplyRepository.java
@@ -1,10 +1,14 @@
 package com.ssafy.forest.repository;
 
+import com.ssafy.forest.domain.entity.ArticleComment;
 import com.ssafy.forest.domain.entity.ArticleCommentReply;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleCommentReplyRepository extends JpaRepository<ArticleCommentReply, Long> {
+
+    List<ArticleCommentReply> findAllByArticleComment(ArticleComment articleComment);
 
 }
