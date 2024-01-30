4c1d0e9f8c094977608e77def7473ce9fe8a98ee
안성재 <sammy0329@naver.com>
2024-01-30 22:14:51 +0900

    feat: findAllByArticleCommentOrderByCreatedAt 메서드로 변경
    - createdAt 빠른 순서대로 답글 가져오기
    - 페이지 작업을 위해 List를 Page로 변경

--- backend/src/main/java/com/ssafy/forest/repository/ArticleCommentReplyRepository.java
+++ backend/src/main/java/com/ssafy/forest/repository/ArticleCommentReplyRepository.java
@@ -2,13 +2,15 @@ package com.ssafy.forest.repository;
 
 import com.ssafy.forest.domain.entity.ArticleComment;
 import com.ssafy.forest.domain.entity.ArticleCommentReply;
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleCommentReplyRepository extends JpaRepository<ArticleCommentReply, Long> {
 
-    List<ArticleCommentReply> findAllByArticleComment(ArticleComment articleComment);
+    Page<ArticleCommentReply> findAllByArticleCommentOrderByCreatedAt(Pageable pageable,
+        ArticleComment articleComment);
 
 }
