37f435544427dc4045d4a44c356bbbdc3e9d885c
안성재 <sammy0329@naver.com>
2024-02-01 00:11:55 +0900

    refactor: ArticleComment 엔티티 member, article에 @ManyToOne(fetch = FetchType.LAZY) 적용

--- backend/src/main/java/com/ssafy/forest/domain/entity/ArticleComment.java
+++ backend/src/main/java/com/ssafy/forest/domain/entity/ArticleComment.java
@@ -3,6 +3,7 @@ package com.ssafy.forest.domain.entity;
 import com.ssafy.forest.domain.dto.request.ArticleCommentReqDto;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -29,11 +30,11 @@ public class ArticleComment extends BaseEntity {
     @Column(name = "comment_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "article_id")
     private Article article;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
