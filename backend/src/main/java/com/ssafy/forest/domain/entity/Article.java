51a21dd8aec24080e1c306f85f4163fd91cd7c20
안성재 <sammy0329@naver.com>
2024-02-01 00:16:13 +0900

    refactor: Article 엔티티에 댓글과 양방향 매핑 설정 및 cascade 설정

--- backend/src/main/java/com/ssafy/forest/domain/entity/Article.java
+++ backend/src/main/java/com/ssafy/forest/domain/entity/Article.java
@@ -1,6 +1,7 @@
 package com.ssafy.forest.domain.entity;
 
 import com.ssafy.forest.domain.dto.request.ArticleReqDto;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,7 +10,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -33,6 +37,9 @@ public class Article extends BaseEntity {
 
     @Column(name = "content", nullable = true, length = 1000)
     private String content;
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ArticleComment> comments = new ArrayList<>();
 
     @Builder
     public Article(Member member, String content) {
