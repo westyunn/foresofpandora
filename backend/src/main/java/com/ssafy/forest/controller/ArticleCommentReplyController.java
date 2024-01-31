a9a98bbb701cb82b4866e053acf665a1a34d9c7c
안성재 <sammy0329@naver.com>
2024-01-31 11:06:31 +0900

    feat: ArticleCommentReplyController쪽 update 메서드 개발

--- backend/src/main/java/com/ssafy/forest/controller/ArticleCommentReplyController.java
+++ backend/src/main/java/com/ssafy/forest/controller/ArticleCommentReplyController.java
@@ -13,6 +13,7 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,16 @@ public class ArticleCommentReplyController {
         @PageableDefault(size = 10) Pageable pageable) {
         return ResponseDto.success(
             articleCommentReplyService.getCommentRepliesByComment(pageable, commentId));
+    }
+
+    @PutMapping("/{commentId}/replies/{replyId}")
+    public ResponseDto<ArticleCommentReplyResDto> update(
+        HttpServletRequest request,
+        @PathVariable Long commentId,
+        @PathVariable Long replyId,
+        @RequestBody ArticleCommentReplyReqDto articleCommentReplyReqDto) {
+        return ResponseDto.success(
+            articleCommentReplyService.update(request, replyId, articleCommentReplyReqDto));
     }
 
 }
