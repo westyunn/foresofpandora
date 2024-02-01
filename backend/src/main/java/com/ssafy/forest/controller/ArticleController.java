413588458955f288ac4a34117a43b1f7fa0f6159
윤정인 <liae100@naver.com>
2024-02-01 09:53:25 +0900

    feat : Swagger 정보 등록
    - ArticleController Swagger 정보 추가
    - MemberController Swagger 정보 추가

--- backend/src/main/java/com/ssafy/forest/controller/ArticleController.java
+++ backend/src/main/java/com/ssafy/forest/controller/ArticleController.java
@@ -6,6 +6,7 @@ import com.ssafy.forest.domain.dto.response.ResponseDto;
 import com.ssafy.forest.service.ArticleService;
 import com.ssafy.forest.service.ReactionService;
 import com.ssafy.forest.service.ReactionServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
@@ -30,7 +31,7 @@ public class ArticleController {
     private final ArticleService articleService;
     private final ReactionService reactionService;
 
-    //게시글 등록
+    @Operation(summary = "게시글 등록", description = "유저가 게시글 등록")
     @PostMapping
     public ResponseDto<ArticleResDto> create(HttpServletRequest request,
         @RequestBody ArticleReqDto articleReqDto) {
@@ -38,7 +39,7 @@ public class ArticleController {
         return ResponseDto.success(createdArticle);
     }
 
-    //게시글 목록 조회
+    @Operation(summary = "게시글 목록 조회", description = "게시글 목록 페이징해서 조회 요청")
     @GetMapping
     public ResponseDto<Page<ArticleResDto>> getList(
         @PageableDefault(size = 15) Pageable pageable
@@ -47,14 +48,14 @@ public class ArticleController {
         return ResponseDto.success(articleList);
     }
 
-    //게시글 단건 조회
+    @Operation(summary = "게시글 단건 조회", description = "게시글 아이디로 조회 요청")
     @GetMapping("/{articleId}")
     public ResponseDto<ArticleResDto> read(@PathVariable Long articleId) {
         ArticleResDto article = articleService.read(articleId);
         return ResponseDto.success(article);
     }
 
-    //게시글 수정
+    @Operation(summary = "게시글 수정", description = "게시글 아이디로 게시글 수정 요청")
     @PutMapping("/{articleId}")
     public ResponseDto<ArticleResDto> update(@PathVariable Long articleId,
         @RequestBody ArticleReqDto articleReqDto, HttpServletRequest request) {
@@ -62,14 +63,14 @@ public class ArticleController {
         return ResponseDto.success(updatedArticle);
     }
 
-    //게시글 삭제
+    @Operation(summary = "게시글 삭제", description = "게시글 아이디로 게시글 삭제 요청")
     @DeleteMapping("/{articleId}")
     public ResponseDto<String> delete(@PathVariable Long articleId, HttpServletRequest request) {
         articleService.delete(articleId, request);
         return ResponseDto.success("SUCCESS");
     }
 
-    //게시글 임시저장
+    @Operation(summary = "게시글 임시저장", description = "유저가 게시글 임시저장")
     @PostMapping("/temp")
     public ResponseDto<ArticleResDto> createTemp(HttpServletRequest request,
         @RequestBody ArticleReqDto articleReqDto) {
@@ -77,14 +78,14 @@ public class ArticleController {
         return ResponseDto.success(articleTemp);
     }
 
-    //임시저장 게시글 단건 조회
+    @Operation(summary = "임시저장 게시글 단건 조회", description = "임시저장 게시글 아이디로 단건 조회 요청")
     @GetMapping("/temp/{tempId}")
     public ResponseDto<ArticleResDto> readTemp(@PathVariable Long tempId) {
         ArticleResDto articleTemp = articleService.readTemp(tempId);
         return ResponseDto.success(articleTemp);
     }
 
-    //임시저장 게시글 등록
+    @Operation(summary = "임시저장 게시글을 게시글로 등록", description = "임시저장 게시글 아이디로 게시글 등록")
     @PostMapping("/temp/{tempId}")
     public ResponseDto<ArticleResDto> createTempToNew(@PathVariable Long tempId,
         @RequestBody ArticleReqDto articleReqDto, HttpServletRequest request) {
@@ -92,7 +93,7 @@ public class ArticleController {
         return ResponseDto.success(articleTemp);
     }
 
-    //임시저장 게시글 수정
+    @Operation(summary = "임시저장 게시글 수정", description = "임시저장 게시글 아이디로 게시글 수정 요청")
     @PutMapping("/temp/{tempId}")
     public ResponseDto<ArticleResDto> updateTemp(@PathVariable Long tempId,
         @RequestBody ArticleReqDto articleReqDto, HttpServletRequest request) {
@@ -100,30 +101,29 @@ public class ArticleController {
         return ResponseDto.success(updatedArticle);
     }
 
-    //임시저장 게시글 삭제
+    @Operation(summary = "임시저장 게시글 삭제", description = "임시저장 게시글 아이디로 게시글 삭제 요청")
     @DeleteMapping("/temp/{tempId}")
     public ResponseDto<String> deleteTemp(@PathVariable Long tempId, HttpServletRequest request) {
         articleService.deleteTemp(tempId, request);
         return ResponseDto.success("SUCCESS");
     }
 
-    //반응 누르기
+    @Operation(summary = "반응 누르기", description = "게시글 아이디로 반응 등록")
     @PostMapping("/reactions/{articleId}")
     public ResponseDto<String> reaction(@PathVariable Long articleId, HttpServletRequest request) {
         reactionService.reaction(articleId, request);
         return ResponseDto.success("SUCCESS");
     }
 
-    //나의 반응 조회
+    @Operation(summary = "나의 반응 조회", description = "게시글 아이디로 나의 반응 조회 요청")
     @GetMapping("/reactions/{articleId}")
     //나의 반응 조회
     public ResponseDto<Boolean> myReaction(@PathVariable Long articleId, HttpServletRequest request) {
         return ResponseDto.success(reactionService.myReaction(articleId, request));
     }
 
-    //게시글 반응 개수 조회
+    @Operation(summary = "게시글 반응 개수 조회", description = "게시글 아이디로 게시글 반응 개수 조회")
     @GetMapping("/reactionCounts/{articleId}")
-    // 게시글 반응 개수 조회
     public ResponseDto<Long> reactionCount(@PathVariable Long articleId, HttpServletRequest request) {
         return ResponseDto.success(reactionService.reactionCount(articleId, request));
     }
