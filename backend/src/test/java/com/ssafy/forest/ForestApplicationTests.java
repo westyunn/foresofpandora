b06038db240bfc3cca93d881ecaa95c0362d4a36
NavyHubb <sngjae972@gmail.com>
2024-02-01 23:08:14 +0900

    fix: @SpringBootTest 주석처리
    [- 빌드 시 contextLoads() FALIED 에러 발생으로 인한 처리]

--- backend/src/test/java/com/ssafy/forest/ForestApplicationTests.java
+++ backend/src/test/java/com/ssafy/forest/ForestApplicationTests.java
@@ -3,7 +3,7 @@ package com.ssafy.forest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ForestApplicationTests {
 
     @Test
