32d314315dd023a3f35c656e7c3dfab3de67dad1
윤정인 <liae100@naver.com>
2024-02-01 09:52:29 +0900

    refactor : ReactionServiceImpl 공백 추가
    - ReactionServiceImpl 공백 추가

--- backend/src/main/java/com/ssafy/forest/service/ReactionServiceImpl.java
+++ backend/src/main/java/com/ssafy/forest/service/ReactionServiceImpl.java
@@ -80,4 +80,5 @@ public class ReactionServiceImpl implements ReactionService {
         return memberRepository.findById(memberFromAccessToken.getId())
             .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER));
     }
+
 }
