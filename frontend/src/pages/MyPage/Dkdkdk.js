45bf0862268f7c929a2d415eb0f8ef14d9553776
유호정 <ys0403ab@naver.com>
2024-02-02 19:40:49 +0900

    [front/mypage] feat : 마이페이지 서버연결

---
+++ frontend/src/pages/MyPage/Dkdkdk.js
@@ -0,0 +1,11 @@
+import { getMyBoard } from "./api";
+
+const Dkdkdk = () => {
+  const getApi = async () => {
+    getMyBoard();
+  };
+
+  return <div onClick={getApi}>dkdkdk</div>;
+};
+
+export default Dkdkdk;
