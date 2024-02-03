9d10165a02e9bc2cce988d13b1f41fb239e86d01
sammy <sammy0329@naver.com>
2024-02-03 18:50:38 +0900

    feat: ArticleTemp와 ArticleImage 양방향 맵핑 진행

--- backend/src/main/java/com/ssafy/forest/domain/entity/ArticleTemp.java
+++ backend/src/main/java/com/ssafy/forest/domain/entity/ArticleTemp.java
@@ -1,6 +1,7 @@
 package com.ssafy.forest.domain.entity;
 
 import com.ssafy.forest.domain.dto.request.ArticleReqDto;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -9,7 +10,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -32,6 +36,9 @@ public class ArticleTemp extends BaseEntity {
 
     @Column(name = "content", nullable = true, length = 1000)
     private String content;
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.REMOVE)
+    private List<ArticleImage> images = new ArrayList<>();
 
     public void update(String content) {
         this.content = content;
