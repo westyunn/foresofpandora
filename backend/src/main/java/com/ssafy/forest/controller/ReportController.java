b81742c266e2deb30319f33b47d1ab7822b3d312
윤정인 <liae100@naver.com>
2024-02-06 00:19:31 +0900

    feat : 신고 기능 ReportController 개발
    - 게시글 신고 기능 개발
    - 댓글 신고 기능 개발
    - 대댓글 신고 기능 개발

---
+++ backend/src/main/java/com/ssafy/forest/controller/ReportController.java
@@ -0,0 +1,40 @@
+package com.ssafy.forest.controller;
+
+import com.ssafy.forest.domain.dto.response.ResponseDto;
+import com.ssafy.forest.service.ReportService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Report API", description = "신고 관련 API")
+@RestController
+@RequestMapping("/api/reports")
+@RequiredArgsConstructor
+public class ReportController {
+
+    private final ReportService reportService;
+
+    @Operation(summary = "게시글 신고", description = "게시글 아이디로 게시글 신고 요청")
+    @PostMapping("/articles/{articleId}")
+    public ResponseDto<String> reportArticle(@PathVariable Long articleId, HttpServletRequest request) {
+        reportService.reportArticle(articleId, request);
+        return ResponseDto.success("SUCCESS");
+    }
+
+    @Operation(summary = "댓글 신고", description = "댓글 아이디로 댓글 신고 요청")
+    @PostMapping("/comments/{commentId}")
+    public ResponseDto<String> reportComment(@PathVariable Long commentId, HttpServletRequest request) {
+        reportService.reportComment(commentId, request);
+        return ResponseDto.success("SUCCESS");
+    }
+
+    @Operation(summary = "대댓글 신고", description = "대댓글 아이디로 대댓글 신고 요청")
+    @PostMapping("/replies/{replyId}")
+    public ResponseDto<String> reportReply(@PathVariable Long replyId, HttpServletRequest request) {
+        reportService.reportReply(replyId, request);
+        return ResponseDto.success("SUCCESS");
+    }
+
+}
