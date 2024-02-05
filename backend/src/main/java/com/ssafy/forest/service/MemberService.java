0c39cc58203e04c826acd020f74de5a8b54be7cc
김성재 <sngjae972@gmail.com>
2024-02-05 11:18:50 +0900

    refactor: service 클래스 추상화 취소

--- backend/src/main/java/com/ssafy/forest/service/MemberService.java
+++ backend/src/main/java/com/ssafy/forest/service/MemberService.java
@@ -2,19 +2,85 @@ package com.ssafy.forest.service;
 
 import com.ssafy.forest.domain.dto.response.ArticleResDto;
 import com.ssafy.forest.domain.dto.response.ArticleTempResDto;
+import com.ssafy.forest.domain.entity.Article;
+import com.ssafy.forest.domain.entity.ArticleTemp;
+import com.ssafy.forest.domain.entity.Member;
+import com.ssafy.forest.domain.entity.Storage;
+import com.ssafy.forest.exception.CustomException;
+import com.ssafy.forest.exception.ErrorCode;
+import com.ssafy.forest.repository.ArticleRepository;
+import com.ssafy.forest.repository.ArticleTempRepository;
+import com.ssafy.forest.repository.MemberRepository;
+import com.ssafy.forest.repository.StorageRepository;
+import com.ssafy.forest.security.TokenProvider;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface MemberService {
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final ArticleRepository articleRepository;
+    private final ArticleTempRepository articleTempRepository;
+    private final StorageRepository storageRepository;
+    private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
+    private final ArticleCommentService articleCommentService;
+    private final ReactionService reactionService;
 
     //내가 작성한 게시글 목록 조회
-    Page<ArticleResDto> getList(Pageable pageable, HttpServletRequest request);
+    public Page<ArticleResDto> getList(Pageable pageable, HttpServletRequest request) {
+        Member member = getMemberFromAccessToken(request);
+        Page<Article> articleList = articleRepository.findByMemberIdOrderByCreatedAtAsc(
+            member.getId(), pageable);
+        return articleList.map(article -> {
+            long commentCount = articleCommentService.getCommentCount(article);
+            long reactionCount = reactionService.countReaction(article.getId());
+            return ArticleResDto.of(article, commentCount, reactionCount);
+        });
+    }
 
     //내가 보관한 게시글 목록 조회
-    Page<ArticleResDto> getStoredList(Pageable pageable, HttpServletRequest request);
+    public Page<ArticleResDto> getStoredList(Pageable pageable, HttpServletRequest request) {
+        Member member = getMemberFromAccessToken(request);
+
+        List<Long> articleIds = storageRepository.findByMemberId(member.getId()).stream()
+            .map(Storage::getArticle)
+            .map(Article::getId)
+            .collect(Collectors.toList());
+
+        Page<Article> storedList = articleRepository.findByIdInOrderByCreatedAtAsc(
+            articleIds, pageable);
+
+        return storedList.map(article -> {
+            long commentCount = articleCommentService.getCommentCount(article);
+            long reactionCount = reactionService.countReaction(article.getId());
+            return ArticleResDto.of(article, commentCount, reactionCount);
+        });
+    }
 
     //내가 임시저장한 게시글 목록 조회
-    Page<ArticleTempResDto> getTempList(Pageable pageable, HttpServletRequest request);
+    public Page<ArticleTempResDto> getTempList(Pageable pageable, HttpServletRequest request) {
+        Member member = getMemberFromAccessToken(request);
+        Page<ArticleTemp> articleTemps = articleTempRepository.findByMemberIdOrderByCreatedAtAsc(
+            member.getId(), pageable);
+        return articleTemps.map(ArticleTempResDto::from);
+    }
+
+    //유저 정보 추출
+    public Member getMemberFromAccessToken(HttpServletRequest request) {
+        // accessToken으로부터 Member 객체 추출
+        Member memberFromAccessToken = tokenProvider.getMemberFromAccessToken(request);
+        // memberFromAccessToken의 id로 최신 상태의 Member 객체 조회
+        return memberRepository.findById(memberFromAccessToken.getId())
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER));
+    }
 
 }
--- backend/src/main/java/com/ssafy/forest/service/StorageService.java
+++ backend/src/main/java/com/ssafy/forest/service/StorageService.java
@@ -1,13 +1,73 @@
 package com.ssafy.forest.service;
 
+import com.ssafy.forest.domain.entity.Article;
+import com.ssafy.forest.domain.entity.Member;
+import com.ssafy.forest.domain.entity.Storage;
+import com.ssafy.forest.exception.CustomException;
+import com.ssafy.forest.exception.ErrorCode;
+import com.ssafy.forest.repository.ArticleRepository;
+import com.ssafy.forest.repository.MemberRepository;
+import com.ssafy.forest.repository.StorageRepository;
+import com.ssafy.forest.security.TokenProvider;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-public interface StorageService {
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class StorageService {
+
+    private final StorageRepository storageRepository;
+    private final ArticleRepository articleRepository;
+    private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
 
     //나의 게시글 보관여부 조회
-    boolean getMyStorage(Long articleId, HttpServletRequest request);
+    public boolean getMyStorage(Long articleId, HttpServletRequest request) {
+        Member member = getMemberFromAccessToken(request);
+
+        if (!articleRepository.existsById(articleId)) {
+            throw new CustomException(ErrorCode.NOT_FOUND_ARTICLE);
+        }
+
+        Storage storage = storageRepository.findByArticleIdAndMemberId(articleId,
+            member.getId()).orElse(null);
+
+        if (storage == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 
     //보관 누르기
-    boolean store(Long articleId, HttpServletRequest request);
+    public boolean store(Long articleId, HttpServletRequest request) {
+        Member member = getMemberFromAccessToken(request);
+
+        Article article = articleRepository.findById(articleId)
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_ARTICLE));
+
+        Storage storage = storageRepository.findByArticleIdAndMemberId(articleId,
+            member.getId()).orElse(null);
+
+        if (storage == null) {
+            storageRepository.save(Storage.from(member, article));
+            return true;
+        } else {
+            storageRepository.deleteById(storage.getId());
+            return false;
+        }
+    }
+
+    //유저 정보 추출
+    public Member getMemberFromAccessToken(HttpServletRequest request) {
+        // accessToken으로부터 Member 객체 추출
+        Member memberFromAccessToken = tokenProvider.getMemberFromAccessToken(request);
+        // memberFromAccessToken의 id로 최신 상태의 Member 객체 조회
+        return memberRepository.findById(memberFromAccessToken.getId())
+            .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER));
+    }
 
 }
