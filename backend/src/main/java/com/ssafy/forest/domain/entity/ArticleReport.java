026d2faefabf815dd80eb2e704d011431024333b
윤정인 <liae100@naver.com>
2024-02-05 17:51:26 +0900

    feat : 신고 게시글 엔티티 ArticleReport 생성
    - 신고 게시글 엔티티 ArticleReport 생성

---
+++ backend/src/main/java/com/ssafy/forest/domain/entity/ArticleReport.java
@@ -0,0 +1,63 @@
+package com.ssafy.forest.domain.entity;
+
+import com.ssafy.forest.domain.dto.request.ArticleReqDto;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "report")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Report extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "report_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "content", nullable = true, length = 1000)
+    private String content;
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.REMOVE)
+    private List<ArticleComment> comments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "article", cascade = CascadeType.REMOVE)
+    private List<ArticleImage> images = new ArrayList<>();
+
+    @Builder
+    public Report(Member member, String content) {
+        this.member = member;
+        this.content = content;
+    }
+
+    public void update(String content) {
+        this.content = content;
+    }
+
+    public static Report from(ArticleReqDto articleReqDto, Member member) {
+        return Report.builder()
+            .member(member)
+            .content(articleReqDto.getContent())
+            .build();
+    }
+
+}
