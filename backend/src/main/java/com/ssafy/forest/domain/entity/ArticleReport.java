f4ca0c3659a83cc75c7f8019457e221f67fc82b4
윤정인 <liae100@naver.com>
2024-02-06 00:14:05 +0900

    feat : 신고 게시글 엔티티 생성, Repository 생성
    - ArticleReport 엔티티 생성
    - ArticleReportRepository 생성

--- backend/src/main/java/com/ssafy/forest/domain/entity/ArticleReport.java
+++ backend/src/main/java/com/ssafy/forest/domain/entity/ArticleReport.java
@@ -1,62 +1,40 @@
 package com.ssafy.forest.domain.entity;
 
-import com.ssafy.forest.domain.dto.request.ArticleReqDto;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "report")
+@Table(name = "articleReport")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Report extends BaseEntity {
+public class ArticleReport extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "report_id")
+    @Column(name = "articleReport_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @Column(name = "content", nullable = true, length = 1000)
-    private String content;
-
-    @OneToMany(mappedBy = "article", cascade = CascadeType.REMOVE)
-    private List<ArticleComment> comments = new ArrayList<>();
-
-    @OneToMany(mappedBy = "article", cascade = CascadeType.REMOVE)
-    private List<ArticleImage> images = new ArrayList<>();
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
 
     @Builder
-    public Report(Member member, String content) {
+    public ArticleReport(Member member, Article article) {
         this.member = member;
-        this.content = content;
+        this.article = article;
     }
 
-    public void update(String content) {
-        this.content = content;
-    }
-
-    public static Report from(ArticleReqDto articleReqDto, Member member) {
-        return Report.builder()
+    public static ArticleReport from(Member member, Article article) {
+        return ArticleReport.builder()
             .member(member)
-            .content(articleReqDto.getContent())
+            .article(article)
             .build();
     }
 
