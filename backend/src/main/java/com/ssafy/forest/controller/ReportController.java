093dea09aaa2484262e73ce07799476135e695f0
윤정인 <liae100@naver.com>
2024-02-06 16:38:21 +0900

    feat : 매개변수 ReportReqDto 추가
    - ReportService 매개변수 ReportReqDto 추가
    - ReportController 매개변수 ReportReqDto 추가

--- backend/src/main/java/com/ssafy/forest/controller/ReportController.java
+++ backend/src/main/java/com/ssafy/forest/controller/ReportController.java
@@ -1,5 +1,6 @@
 package com.ssafy.forest.controller;
 
+import com.ssafy.forest.domain.dto.request.ReportReqDto;
 import com.ssafy.forest.domain.dto.response.ResponseDto;
 import com.ssafy.forest.service.ReportService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -18,22 +19,28 @@ public class ReportController {
 
     @Operation(summary = "게시글 신고", description = "게시글 아이디로 게시글 신고 요청")
     @PostMapping("/articles/{articleId}")
-    public ResponseDto<String> reportArticle(HttpServletRequest request, @PathVariable Long articleId) {
-        reportService.reportArticle(request, articleId);
+    public ResponseDto<String> reportArticle(
+        HttpServletRequest request, @PathVariable Long articleId,
+        @RequestBody ReportReqDto reportReqDto) {
+        reportService.reportArticle(request, articleId, reportReqDto);
         return ResponseDto.success("SUCCESS");
     }
 
     @Operation(summary = "댓글 신고", description = "댓글 아이디로 댓글 신고 요청")
     @PostMapping("/comments/{commentId}")
-    public ResponseDto<String> reportComment(HttpServletRequest request, @PathVariable Long commentId) {
-        reportService.reportComment(request, commentId);
+    public ResponseDto<String> reportComment(
+        HttpServletRequest request, @PathVariable Long commentId,
+        @RequestBody ReportReqDto reportReqDto) {
+        reportService.reportComment(request, commentId, reportReqDto);
         return ResponseDto.success("SUCCESS");
     }
 
     @Operation(summary = "대댓글 신고", description = "대댓글 아이디로 대댓글 신고 요청")
     @PostMapping("/replies/{replyId}")
-    public ResponseDto<String> reportReply(HttpServletRequest request, @PathVariable Long replyId) {
-        reportService.reportReply(request, replyId);
+    public ResponseDto<String> reportReply(
+        HttpServletRequest request, @PathVariable Long replyId,
+        @RequestBody ReportReqDto reportReqDto) {
+        reportService.reportReply(request, replyId, reportReqDto);
         return ResponseDto.success("SUCCESS");
     }
 
