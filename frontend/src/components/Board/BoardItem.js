019cc30e50f0ae39e4578d2dc1c32e827525a974
유호정 <ys0403ab@naver.com>
2024-02-06 21:07:39 +0900

    [front/main/update] feat: 게시글 수정 구현, 임시저장글 게시글로 등록 구현

--- frontend/src/components/Board/BoardItem.js
+++ frontend/src/components/Board/BoardItem.js
@@ -48,7 +48,13 @@ const BoardItem = ({ item, type }) => {
   }
 
   const getRepImg = () => {
-    setImg(item.imageList[0]);
+    if (item.imageList.length === 0) {
+      setImg(
+        "https://images.unsplash.com/photo-1592769606534-fe78d27bf450?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+      );
+    } else {
+      setImg(item.imageList[0]);
+    }
   };
 
   const getDetail = async (id) => {
--- frontend/src/pages/Main/Board/BoardUpdate.js
+++ frontend/src/pages/Main/Board/BoardUpdate.js
@@ -6,7 +6,11 @@ import { Link } from "react-router-dom";
 
 import style from "./BoardCreate.module.css";
 import ImageButton from "../../../assets/BoardCreateImage.png";
-import { updateMyTemp, postMyTemp } from "../BoardTemp/api";
+import {
+  updateMyTemp,
+  postMyTemp,
+  postTempToMyArticle,
+} from "../BoardTemp/api";
 import { putMyArticle } from "./api";
 
 const BoardCreate = () => {
@@ -17,13 +21,13 @@ const BoardCreate = () => {
 
   // 글
   const [board, setBoard] = useState(state.item.content);
-  // const imgUrls = state.item.imgUrls
   const imgUrls = [
-    "https://images.unsplash.com/photo-1518288774672-b94e808873ff?q=80&w=1938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1606225457115-9b0de873c5db?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1592769606534-fe78d27bf450?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "https://images.unsplash.com/photo-1537151608828-ea2b11777ee8?q=80&w=1894&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   ];
+  // const imgUrls = state.item.imageList;
   const id = state.item.id;
-  console.log(state.temp);
+  console.log(state.item.id);
 
   //임시저장 수정(임시저장에서 글 가져와서 다시 임시저장 하기)
   const updateTemp = async (id) => {
@@ -38,9 +42,9 @@ const BoardCreate = () => {
     }
   };
   //임시저장 등록(수정글 가져와서 임시저장글로 등록)
-  const postTemp = async (id) => {
+  const postTemp = async (board) => {
     try {
-      const data = await postMyTemp(id, board);
+      const data = await postMyTemp(board);
       // console.log(item);
       console.log("click", data);
       alert("임시저장이 완료되었습니다!");
@@ -52,6 +56,7 @@ const BoardCreate = () => {
   //게시글 수정
   const putArticle = async (id) => {
     try {
+      console.log(board);
       const data = await putMyArticle(id, board);
       // console.log(item);
       console.log("click", data);
@@ -62,16 +67,16 @@ const BoardCreate = () => {
     }
   };
   //임시저장 글 게시글로 등록
-  const postToArticleFromTemp = async (id) => {
-    // try {
-    //   const data = await putMyArticle(id, board);
-    //   // console.log(item);
-    //   console.log("click", data);
-    //   alert("임시저장이 완료되었습니다!");
-    //   navigator("/boardtemp");
-    // } catch (error) {
-    //   console.error("Error deleting temp:", error);
-    // }
+  const postTempToArticle = async (id) => {
+    try {
+      const data = await postTempToMyArticle(id, board);
+      // console.log(item);
+      console.log("click", data);
+      alert("업로드가 완료되었습니다!");
+      navigator("/");
+    } catch (error) {
+      console.error("Error deleting temp:", error);
+    }
   };
 
   const handleTextareaChange = (event) => {
@@ -101,33 +106,32 @@ const BoardCreate = () => {
                 updateTemp(id);
               }}
             >
-              임시저장
+              임시저장 |
             </button>
           )}
+
           {state.temp && (
-            <button
-              className={`${style.bt_save}`}
-              onClick={() => {
-                postTemp(id);
-              }}
-            >
-              임시저장
-            </button>
+            <Link to="/boardtemp">
+              <div>임시보관함</div>
+            </Link>
           )}
-          |
-          <Link to="/boardtemp">
-            <div>임시보관함</div>
-          </Link>
           {state.temp && (
             <button
               className={`${style.bt_upload}`}
-              onClick={postToArticleFromTemp}
+              onClick={() => {
+                postTempToArticle(id);
+              }}
             >
               업로드
             </button>
           )}
           {!state.temp && (
-            <button className={`${style.bt_upload}`} onClick={putArticle}>
+            <button
+              className={`${style.bt_upload}`}
+              onClick={() => {
+                putArticle(id);
+              }}
+            >
               수정
             </button>
           )}
