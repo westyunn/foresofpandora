c75982aaccc3f594eae4f7a8e0c3dc95bc9e7ae4
김성재 <sngjae972@gmail.com>
2024-02-07 13:56:17 +0900

    refactor: 미사용 코드 삭제

--- backend/src/main/java/com/ssafy/forest/ForestApplication.java
+++ backend/src/main/java/com/ssafy/forest/ForestApplication.java
@@ -1,7 +1,5 @@
 package com.ssafy.forest;
 
-import jakarta.annotation.PostConstruct;
-import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -12,12 +10,6 @@ public class ForestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ForestApplication.class, args);
-    }
-
-    @PostConstruct
-    public void setTimeZone() {
-        // timezone UTC 셋팅
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 
 }
