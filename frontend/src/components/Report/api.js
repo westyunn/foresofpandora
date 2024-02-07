66a4eaddd9164b4f82068eb7a3d9e3b945946e09
유호정 <ys0403ab@naver.com>
2024-02-07 14:11:21 +0900

    [front/main/report] feat : 게시글 신고 구현

---
+++ frontend/src/components/Report/api.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+// const token = localStorage.getItem("access_token");
+// const refreshToken = localStorage.getItem("refresh_token");
+
+const token =
+  "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxIiwiYXV0aCI6IlJPTEVfTUVNQkVSIiwiZXhwIjoxNzA3MzY3NDUzfQ.WvRvgBcp0HsGXmGa9ueZXnooSlPdyMm9bZ1HcRVjopw";
+const refreshToken =
+  "eyJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3MDc4ODU4NTN9.kKEbig6-rbbjCRzMH-cDcN4hJQtcbNeTlPwkFPQQx-Q";
+
+export async function reportArticle(id, content) {
+  try {
+    const data = { content: content };
+    console.log("data", data);
+    if (token) {
+      const res = await axios({
+        method: "POST",
+        url: `/api/reports/articles/${id}`,
+        data,
+        // access token이랑 refresh token 둘 다 req header에 담아서 보냅니당
+        headers: {
+          authorization: `Bearer ${token}`,
+          refreshtoken: refreshToken,
+        },
+      });
+      console.log(res);
+      return res;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+export async function reportComment(id, content) {
+  try {
+    const data = { content: content };
+    console.log("data", data);
+    if (token) {
+      const res = await axios({
+        method: "POST",
+        url: `/api/reports/comments/${id}`,
+        data,
+        // access token이랑 refresh token 둘 다 req header에 담아서 보냅니당
+        headers: {
+          authorization: `Bearer ${token}`,
+          refreshtoken: refreshToken,
+        },
+      });
+      return res;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+export async function reportReply(id, content) {
+  try {
+    const data = { content: content };
+    console.log("data", data);
+    if (token) {
+      const res = await axios({
+        method: "POST",
+        url: `/api/reports/replies/${id}`,
+        data,
+        // access token이랑 refresh token 둘 다 req header에 담아서 보냅니당
+        headers: {
+          authorization: `Bearer ${token}`,
+          refreshtoken: refreshToken,
+        },
+      });
+      return res;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
