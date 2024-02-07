2334ff953ddd55693e5c4b1604c27d9f50e5f329
안성재 <sammy0329@naver.com>
2024-02-07 22:10:20 +0900

    refactor: 댓글 목록 조회 메서드 수정
    [댓글 목록 조회시 답글수를 가져오기 위해 수많은 쿼리가 추가적으로 나가는 이슈 해결을 위해 게시글 조회하는 쿼리를 실행하고, 조회된 게시글들과 각 게시글의 댓글 수를 함께 반환하도록 쿼리 수정]

--- backend/src/main/java/com/ssafy/forest/repository/ArticleCommentRepository.java
+++ backend/src/main/java/com/ssafy/forest/repository/ArticleCommentRepository.java
@@ -6,13 +6,23 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
 
-    Page<ArticleComment> findAllByArticleAndDeletedAtIsNullOrderByCreatedAt(Pageable pageable,
-        Article article);
+    @Query("SELECT c, COUNT(r) " +
+        "FROM ArticleComment c " +
+        "LEFT JOIN c.replies r " +
+        "WHERE c.article = :article " +
+        "AND c.deletedAt IS NULL " +
+        "AND c.article.isArticle = true " +
+        "AND c.article.deletedAt IS NULL " +
+        "GROUP BY c")
+    Page<Object[]> findAllWithReplyCountByArticle(Pageable pageable,
+        @Param("article") Article article);
 
     Optional<ArticleComment> findByIdAndDeletedAtIsNullAndArticleId(long commentId, long articleId);
 
