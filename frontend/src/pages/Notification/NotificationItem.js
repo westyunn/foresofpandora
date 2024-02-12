7c0a465371e02e6634d23c04672555c49153ee89
조연주 <yjyk9679@naver.com>
2024-02-13 08:56:27 +0900

    [front/notification] feat: 알림 리덕스, 아이콘 추가

--- frontend/src/pages/Notification/NotificationItem.js
+++ frontend/src/pages/Notification/NotificationItem.js
@@ -1,18 +1,29 @@
 import style from "./NotificationItem.module.css";
 
-const NotificationItem = () => {
-  const ArticleContent = "집에 가고 싶다";
-  const content = "나도 집에 가고 싶다";
+import notice_comment from "../../assets/notice_comment.png";
+import notice_like from "../../assets/notice_like.png";
 
+const NotificationItem = ({ event, myContent, content, time }) => {
   return (
     <div className={`${style.container}`}>
       <div className={`${style.icon_notice}`}>
-        <div className={`${style.icon}`}>아이콘</div>
+        <div className={`${style.icon}`}>
+          {event === "comment" && <img src={notice_comment} />}
+          {event === "like" && <img src={notice_like} />}
+        </div>
         <div className={`${style.notice}`}>
-          <div className={`${style.articleContent}`}>
-            내 글"{ArticleContent}"에 댓글이 달렸습니다
-          </div>
-          <div className={`${style.content}`}>{content}</div>
+          {event === "comment" && (
+            <div className={`${style.articleContent}`}>
+              내 글에 댓글 "{content}..."이 달렸습니다
+            </div>
+          )}
+          {event === "like" && (
+            <div className={`${style.articleContent}`}>
+              내 글에 좋아요가 달렸습니다
+            </div>
+          )}
+
+          <div className={`${style.content}`}>{myContent}</div>
         </div>
       </div>
       <div className={`${style.time}`}>3분전</div>
