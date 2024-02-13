ce36bd256b7b9f9122c7f64587b554a9f7bc4aa6
AnSeongJae <sammy0329@naver.com>
2024-02-13 15:20:26 +0900

    feat: 좋아요 클릭시 알람 로직 추가
    [좋아요가 이미 존재하고, deletedAt이 null 일 때
    좋아요가 이미 존재하고, deletedAt이 null이 아닐 때
    좋아요가 존재하지 않을 때
    구분해서 개발]

--- backend/src/main/java/com/ssafy/forest/service/ReactionService.java
+++ backend/src/main/java/com/ssafy/forest/service/ReactionService.java
@@ -1,15 +1,20 @@
 package com.ssafy.forest.service;
 
+import com.ssafy.forest.domain.AlarmArgs;
+import com.ssafy.forest.domain.entity.Alarm;
 import com.ssafy.forest.domain.entity.Article;
 import com.ssafy.forest.domain.entity.Member;
 import com.ssafy.forest.domain.entity.Reaction;
+import com.ssafy.forest.domain.type.AlarmType;
 import com.ssafy.forest.exception.CustomException;
 import com.ssafy.forest.exception.ErrorCode;
+import com.ssafy.forest.repository.AlarmRepository;
 import com.ssafy.forest.repository.ArticleRepository;
 import com.ssafy.forest.repository.MemberRepository;
 import com.ssafy.forest.repository.ReactionRepository;
 import com.ssafy.forest.security.TokenProvider;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,6 +27,7 @@ public class ReactionService {
     private final ReactionRepository reactionRepository;
     private final ArticleRepository articleRepository;
     private final MemberRepository memberRepository;
+    private final AlarmRepository alarmRepository;
     private final TokenProvider tokenProvider;
 
     //반응 누르기
@@ -31,16 +37,54 @@ public class ReactionService {
         Article article = articleRepository.findByIdAndIsArticleIsTrueAndDeletedAtIsNull(articleId)
             .orElseThrow(() -> new CustomException(ErrorCode.INVALID_RESOURCE));
 
-        Reaction reaction = reactionRepository.findByArticleIdAndMemberId(articleId,
-            member.getId()).orElse(null);
+        Optional<Reaction> reaction = reactionRepository.findByArticleIdAndMemberId(articleId,
+            member.getId());
 
-        if (reaction == null) {
-            reactionRepository.save(Reaction.from(member, article));
-            return true;
-        } else {
-            reactionRepository.deleteById(reaction.getId());
+        // 좋아요가 이미 존재하고, deletedAt이 null 일 때 - 삭제되지 않은 상태
+        if (reaction.isPresent() && reaction.get().getDeletedAt() == null) {
+            reactionRepository.deleteById(reaction.get().getId());
+            if (reaction.get().getMember() != article.getMember()) {
+                Optional<Alarm> alarm = alarmRepository.findAlarmByMemberAndArticleId(article.getMember(),
+                    article.getId());
+                if (alarm.isPresent() && alarm.get().getDeletedAt() == null) {
+                    alarmRepository.delete(alarm.get());
+                }
+            }
             return false;
         }
+
+        // 좋아요가 이미 존재하고, deletedAt이 null이 아닐 때 - 삭제된 상태
+        if (reaction.isPresent() && reaction.get().getDeletedAt() != null) {
+            reaction.get().resetDeletedAt(reaction.get());
+            if (reaction.get().getMember() != article.getMember()) {
+                Optional<Alarm> alarm = alarmRepository.findAlarmByMemberAndArticleId(article.getMember(),
+                    article.getId());
+                if (alarm.isPresent() && alarm.get().getDeletedAt() != null) {
+                    alarm.get().resetDeletedAt(alarm.get());
+                }
+                if (alarm.isEmpty()) {
+                    alarmRepository.save(
+                        Alarm.of(article.getMember(), AlarmType.NEW_REACTION_ON_ARTICLE,
+                            new AlarmArgs(member.getId(), article.getId())));
+                }
+            }
+            return true;
+        }
+
+        // 좋아요가 존재하지 않을 때
+        Reaction createdreaction = reactionRepository.save(Reaction.from(member, article));
+        if (createdreaction.getMember() != article.getMember()) {
+            Optional<Alarm> alarm = alarmRepository.findAlarmByMemberAndArticleId(article.getMember(),
+                article.getId());
+            if (alarm.isPresent() && alarm.get().getDeletedAt() != null) {
+                alarm.get().resetDeletedAt(alarm.get());
+            }
+            if (alarm.isEmpty()) {
+                alarmRepository.save(Alarm.of(article.getMember(), AlarmType.NEW_REACTION_ON_ARTICLE,
+                    new AlarmArgs(member.getId(), article.getId())));
+            }
+        }
+        return true;
     }
 
     //나의 반응 조회
