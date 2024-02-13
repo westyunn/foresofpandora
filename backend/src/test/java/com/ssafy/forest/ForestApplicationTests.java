32212a839125be4cf9cc75115c76e69966ad0511
김성재 <sngjae972@gmail.com>
2024-02-13 21:45:40 +0900

    refactor: 배포 환경 설정

--- backend/src/test/java/com/ssafy/forest/ForestApplicationTests.java
+++ backend/src/test/java/com/ssafy/forest/ForestApplicationTests.java
@@ -1,9 +1,8 @@
 package com.ssafy.forest;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ForestApplicationTests {
 
     @Test
