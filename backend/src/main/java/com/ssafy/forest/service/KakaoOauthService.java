fc7497e69ca93a28c36a0642b2bbaba6215c9104
김성재 <sngjae972@gmail.com>
2024-02-13 21:24:43 +0900

    refactor: 프론트 로컬 개발 용 kakaoRedirecUrl 설정

--- backend/src/main/java/com/ssafy/forest/service/KakaoOauthService.java
+++ backend/src/main/java/com/ssafy/forest/service/KakaoOauthService.java
@@ -95,8 +95,8 @@ public class KakaoOauthService {
     private TokenDto getAccessToken(String code, String mode) {
         String redirectUrl;
         if (mode.equals("login")) {
-           redirectUrl = "http://localhost:8080/auth/success";  // 백 서버
-            // redirectUrl = "http://localhost:3000/auth/kakao";  // 프론트 서버
+//           redirectUrl = "http://localhost:8080/auth/success";  // 백 서버
+             redirectUrl = "http://localhost:3000/auth/kakao";  // 프론트 서버
         }
         else {
            redirectUrl = "http://localhost:8080/auth/kakaologout";  // 로컬 서버
