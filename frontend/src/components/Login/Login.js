321f529c7612f56561b39cbac5a5bf1c9a1b58df
이서윤 <s2yunn@gmail.com>
2024-02-13 17:07:46 +0900

    [front/login] feat: 로그인 취소

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,11 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { userActions } from "../../store/user";
 import { useSelector } from "react-redux";
 import styles from "./Login.module.css";
 const Login = ({ children }) => {
   // 로그인이 필요한 화면
   const isLoggedIn = useSelector((state) => state.user.isLoggedIn);
-
+  const navigate = useNavigate();
+  const cancelHandler = () => {
+    navigate(-1);
+  };
   if (!isLoggedIn) {
     return (
       <div className={styles.loginContainer}>
@@ -15,6 +18,9 @@ const Login = ({ children }) => {
             로그인하기{" "}
           </Link>
         </div>
+        <button className={styles.login_cancel_btn} onClick={cancelHandler}>
+          취소
+        </button>
       </div>
     );
   }
