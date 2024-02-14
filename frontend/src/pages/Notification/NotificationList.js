5a806aa9763385dbf9bbe66b2f8eb1cd84e09ff2
조연주 <yjyk9679@naver.com>
2024-02-14 23:01:26 +0900

    [front/notification] 알림 페이지 작업

--- frontend/src/pages/Notification/NotificationList.js
+++ frontend/src/pages/Notification/NotificationList.js
@@ -1,31 +1,45 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import axios from "axios";
 
 import NotificationItem from "./NotificationItem";
 import style from "./NotificationList.module.css";
 
 const NotificationList = () => {
-  const noticeList = useSelector((state) => state.notification.noticeList);
+  const page = 0; // test - 수정 필요
 
-  const testList = [
-    {
-      id: 1,
-      event: "comment",
-      myContent: "test",
-      content: "test2",
-      time: 3,
-    },
-    {
-      id: 2,
-      event: "like",
-      myContent: "test",
-      content: "test2",
-      time: 5,
-    },
-  ];
+  const token = localStorage.getItem("access_token");
+  const refreshToken = localStorage.getItem("refresh_token");
+
+  const [noticeList, setNoticeList] = useState([]);
+
+  useEffect(() => {
+    getNoticeList();
+  }, [page]);
+
+  // axios : 알림 목록 조회
+  const getNoticeList = () => {
+    axios
+      .get(`/api/member/alarm`, {
+        params: {
+          page,
+        },
+        headers: {
+          authorization: `Bearer ${token}`,
+          refreshtoken: refreshToken,
+        },
+      })
+      .then((response) => {
+        setNoticeList(response.data.data.content);
+      })
+      .catch((err) => {
+        console.log("알림 불러오기 실패 : ", err);
+      });
+  };
 
   return (
     <div className={`${style.container}`}>
-      {testList.map((it) => (
+      {noticeList.map((it) => (
         <NotificationItem key={it.id} {...it} />
       ))}
     </div>
