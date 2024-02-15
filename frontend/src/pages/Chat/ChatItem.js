279a2ab758f8360e3cbbfe920998298735cf1927
유호정 <ys0403ab@naver.com>
2024-02-15 23:14:15 +0900

    [front/chatting] fix : 채팅수정

--- frontend/src/pages/Chat/ChatItem.js
+++ frontend/src/pages/Chat/ChatItem.js
@@ -1,14 +1,23 @@
 import { Link, useNavigate } from "react-router-dom";
+import { profileImg } from "../../components/profileImg";
 
 import temp_profile from "../../assets/cat1.png";
 import style from "./ChatItem.module.css";
 
 const ChatItem = ({ roomId }) => {
+  // 랜덤 인덱스 생성 (프로필 이미지)
+  const profileIdx = Math.floor(Math.random() * profileImg.length - 1);
+
+  // 랜덤 인덱스 생성 (프로필 이미지 배경)
+  const colorIdx = Math.floor(Math.random() * 2);
+
   // console.log("item", roomId);
   const navigator = useNavigate();
   // console.log(room);
   const getDetail = () => {
-    navigator(`/chat/${roomId}`, { state: { roomId: roomId } });
+    navigator(`/chat/${roomId}`, {
+      state: { roomId: roomId, profileIdx: profileIdx, colorIdx: colorIdx },
+    });
   };
 
   const id = 0; // test
@@ -17,7 +26,23 @@ const ChatItem = ({ roomId }) => {
     <div onClick={getDetail}>
       <div className={`${style.chat_item}`}>
         <div className={`${style.left_side}`}>
-          <img src={temp_profile} className={`${style.profile}`} />
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              background: profileImg[profileIdx].color[colorIdx],
+              borderRadius: "100%",
+              width: "4rem",
+              height: "4rem",
+            }}
+          >
+            <img
+              src={profileImg[profileIdx].image}
+              style={{ width: "3rem" }}
+            ></img>
+          </div>
+          {/* <img src={temp_profile} className={`${style.profile}`} /> */}
           <div className={`${style.current_message}`}>
             {roomId}번 채팅방 입니다
           </div>
