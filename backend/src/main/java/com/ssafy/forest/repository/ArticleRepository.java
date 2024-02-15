cc1d41597885685572272ddd46eff0c7a6d77376
김성재 <sngjae972@gmail.com>
2024-02-16 03:25:04 +0900

    백엔드 정리

--- backend/src/main/java/com/ssafy/forest/repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.forest.repository;
-
-import com.ssafy.forest.domain.entity.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-
-}
