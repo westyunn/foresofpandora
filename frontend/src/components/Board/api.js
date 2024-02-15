b21c045ea0fe9032fe47984133adf448e2606ab3
유호정 <ys0403ab@naver.com>
2024-02-15 12:57:25 +0900

    [front/main] fix : 서버 연결 시 예외처리, 탈퇴한 회원 게시글 신고 막기

--- frontend/src/components/Board/api.js
+++ frontend/src/components/Board/api.js
@@ -4,18 +4,22 @@ export async function getMyBoard(page) {
   const token = localStorage.getItem("access_token");
   const refreshToken = localStorage.getItem("refresh_token");
   const params = { page: page };
-  if (token) {
-    const res = await axios({
-      method: "GET",
-      url: `/api/member/articles`,
-      params,
-      // access token이랑 refresh token 둘 다 req header에 담아서 보냅니당
-      headers: {
-        authorization: `Bearer ${token}`,
-        refreshtoken: refreshToken,
-      },
-    });
-    return res;
+  try {
+    if (token) {
+      const res = await axios({
+        method: "GET",
+        url: `/api/member/articles`,
+        params,
+        // access token이랑 refresh token 둘 다 req header에 담아서 보냅니당
+        headers: {
+          authorization: `Bearer ${token}`,
+          refreshtoken: refreshToken,
+        },
+      });
+      return res;
+    }
+  } catch (err) {
+    console.log("내가 쓴 글 불러오기 실패", err);
   }
 }
 
@@ -23,35 +27,43 @@ export async function getMySaved(page) {
   const token = localStorage.getItem("access_token");
   const refreshToken = localStorage.getItem("refresh_token");
   const params = { page: page };
-  if (token) {
-    const res = await axios({
-      method: "GET",
-      url: `/api/member/storages`,
-      params,
-      // access token이랑 refresh token 둘 다 req header에 담아서 보냅니당
-      headers: {
-        authorization: `Bearer ${token}`,
-        refreshtoken: refreshToken,
-      },
-    });
-    return res;
+  try {
+    if (token) {
+      const res = await axios({
+        method: "GET",
+        url: `/api/member/storages`,
+        params,
+        // access token이랑 refresh token 둘 다 req header에 담아서 보냅니당
+        headers: {
+          authorization: `Bearer ${token}`,
+          refreshtoken: refreshToken,
+        },
+      });
+      return res;
+    }
+  } catch (err) {
+    console.log("보관한글 불러오기 실패", err);
   }
 }
 
 export async function getBoardDetail(id) {
   const token = localStorage.getItem("access_token");
   const refreshToken = localStorage.getItem("refresh_token");
-  if (token) {
-    const res = await axios({
-      method: "GET",
-      url: `/api/articles/${id}`,
-      // access token이랑 refresh token 둘 다 req header에 담아서 보냅니당
-      headers: {
-        authorization: `Bearer ${token}`,
-        refreshtoken: refreshToken,
-      },
-    });
-    const data = res.data.data;
-    return data;
+  try {
+    if (token) {
+      const res = await axios({
+        method: "GET",
+        url: `/api/articles/${id}`,
+        // access token이랑 refresh token 둘 다 req header에 담아서 보냅니당
+        headers: {
+          authorization: `Bearer ${token}`,
+          refreshtoken: refreshToken,
+        },
+      });
+      const data = res.data.data;
+      return data;
+    }
+  } catch (err) {
+    console.log("게시글 상세 조회 실패", err);
   }
 }
